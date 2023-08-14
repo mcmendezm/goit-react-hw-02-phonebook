4f899ce43914b334c4358d5e730ef5fd09a4107f
Camila Mendez <mcmendezmanotas@gmail.com>
2023-08-13 22:08:52 -0500

    adicionando modificaciones solicitadas

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,89 +1,11 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-
-const ContactForm = ({ name, number, onChange, onSubmit }) => (
-  <form onSubmit={onSubmit}>
-    <label>
-      Name:
-      <input
-        type="text"
-        name="name"
-        value={name}
-        onChange={onChange}
-        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-        title="Name may contain only letters, apostrophe, dash and spaces."
-        required
-      />
-    </label>
-    <br />
-    <label>
-      Number:
-      <input
-        style={{ margin: '9px' }}
-        type="tel"
-        name="number"
-        value={number}
-        onChange={onChange}
-        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-        required
-      />
-    </label>
-    <br />
-    <button
-      type="submit"
-      style={{
-        margin: '9px',
-        backgroundColor: '#2874A6 ',
-        color: '#fff',
-        padding: '5px 8px',
-        border: 'none',
-        borderRadius: '20px',
-        cursor: 'pointer',
-      }}
-    >
-      Add Contact
-    </button>
-  </form>
-);
-
-const ContactList = ({ contacts, onDeleteContact }) => (
-  <ul>
-    {contacts.map(contact => (
-      <li key={contact.id}>
-        {contact.name}: {contact.number}
-        <button
-          style={{
-            margin: '9px',
-            backgroundColor: '#2874A6 ',
-            color: '#fff',
-            padding: '5px 8px',
-            border: 'none',
-            borderRadius: '20px',
-            cursor: 'pointer',
-          }}
-          onClick={() => onDeleteContact(contact.id)}
-        >
-          Delete
-        </button>
-      </li>
-    ))}
-  </ul>
-);
-
-const Filter = ({ filter, onChange }) => (
-  <label>
-    Filter by name:
-    <input type="text" value={filter} onChange={onChange} />
-  </label>
-);
-
+import ContactForm from './ContactForm';
+import ContactList from './ContactList';
+import Filter from './Filter';
 class App extends Component {
   state = {
     contacts: [],
     filter: '',
-    name: '',
-    number: '',
   };
 
   handleChange = e => {
@@ -99,14 +21,11 @@ class App extends Component {
       const newContact = { id: this.generateUniqueId(), name, number };
       this.setState(prevState => ({
         contacts: [...prevState.contacts, newContact],
-        name: '',
-        number: '',
       }));
     } else {
       alert(`Contact "${name}" already exists!`);
     }
   };
-
   generateUniqueId = () => {
     return Date.now().toString();
   };
@@ -126,7 +45,7 @@ class App extends Component {
   };
 
   render() {
-    const { contacts, filter, name, number } = this.state;
+    const { contacts, filter } = this.state;
     const filteredContacts = contacts.filter(contact =>
       contact.name.toLowerCase().includes(filter.toLowerCase())
     );
@@ -135,8 +54,6 @@ class App extends Component {
       <div>
         <h1 style={{ color: '#2874A6' }}>Phonebook</h1>
         <ContactForm
-          name={name}
-          number={number}
           onChange={this.handleChange}
           onSubmit={this.handleAddContact}
         />
@@ -153,25 +70,3 @@ class App extends Component {
 }
 
 export default App;
-
-ContactForm.propTypes = {
-  name: PropTypes.string.isRequired,
-  number: PropTypes.string.isRequired,
-  onChange: PropTypes.func.isRequired,
-  onSubmit: PropTypes.func.isRequired,
-};
-ContactList.propTypes = {
-  contacts: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.string.isRequired,
-      name: PropTypes.string.isRequired,
-      number: PropTypes.string.isRequired,
-    })
-  ).isRequired,
-  onDeleteContact: PropTypes.func.isRequired,
-};
-
-Filter.propTypes = {
-  filter: PropTypes.string.isRequired,
-  onChange: PropTypes.func.isRequired,
-};
